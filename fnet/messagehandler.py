77a69f0a462cffc0322f4f5dd6058db1a5d6ad46
FRANLV <1504069568@qq.com>
2022-09-05 22:12:42 +0800

    repeated id check

--- fnet/messagehandler.py
+++ fnet/messagehandler.py
@@ -1,10 +1,12 @@
+from typing import Dict
+
 from fnet.router import Router
 from fnet.tcprequest import TcpRequest
 
 
 class MessageHandler:
     def __init__(self):
-        self.apis = {}  # key msgId, value router00
+        self.apis: Dict[int, Router] = {}  # key msgId, value router
 
     async def process_messages_now(self, req: TcpRequest):
         router: Router = self.apis.get(req.msgId)  # get router
@@ -22,5 +24,7 @@ class MessageHandler:
         :param router:
         :return:
         """
+        if msgId in self.apis is True:
+            raise Exception(f"repeated api {msgId}")
         self.apis[msgId] = router
         print(f"add router success,msgId is {msgId}")
